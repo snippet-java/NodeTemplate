50125dd197000472f1840ffe841eb0f2168ea266
snippet-java <snippet.java@gmail.com>
2016-10-10 16:32:18 +0800

    Update snippet.js

--- src/snippet.js
+++ src/snippet.js
@@ -9,9 +9,8 @@ var parameters = {};
 // Output will be reflected via console.log function  //
 ////////////////////////////////////////////////////////
 function process(req_parameters, callback) {
-  
-  {{code}}
-  
+	// actual code will be pasted here
+	{{code}}  
 }
 
 ////////////////////////////////////////////////////////
@@ -21,21 +20,22 @@ function process(req_parameters, callback) {
 if (require.main === module) {
 	process(parameters,null);
 } else {
-
-  // name of the unit for logging and servlet path also
+	
+	// name of the unit for logging 
+	// and servlet path also
 	var unitpath = "";
-
-  // Template for making above code available
-  // as service via superglue routine
+	
+	// Template for making above code available
+	// as service via superglue routine
 	var superglue = require('sandbox-superglue');
 	module.exports = {
-    path: '/'+unitpath,
-    priority: 1,
-    
-    init: function (app) {
-      // something to do initially
-    },
-    GET:  function(req, res) {superglue.GET(req,res,parameters,unitpath)},
-    POST: function(req, res) {superglue.POST(req,res,process)}
+		path: '/'+unitpath,
+		priority: 1,
+		
+		init: function (app) {
+			// something to do initially
+		},
+		GET:  function(req, res) {superglue.GET(req,res,parameters,unitpath)},
+		POST: function(req, res) {superglue.POST(req,res,process)}
 	}
 }
