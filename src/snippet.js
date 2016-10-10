c0ecee02e02c145988b5719ca6f9545ba5126078
snippet-java <snippet.java@gmail.com>
2016-10-10 11:33:53 -0400

    update snippet.js

--- src/snippet.js
+++ src/snippet.js
@@ -10,7 +10,36 @@ var parameters = {};
 ////////////////////////////////////////////////////////
 function process(req_parameters, callback) {
 	// actual code will be pasted here
-	{{code}}  
+	'use strict';
+var watson = require('watson-developer-cloud');
+var fs     = require('fs');
+
+var personality_insights = watson.personality_insights({
+  username: "", // SET YOUR USERNAME
+  password: "", // SET YOUR PASSWORD
+  version: "v2"
+});
+
+personality_insights.profile({
+  text: "Call me Ishmael. Some years ago-never mind how long "
+      + "precisely-having little or no money in my purse, and nothing "
+      + "particular to interest me on shore, I thought I would sail about "
+      + "a little and see the watery part of the world. It is a way "
+      + "I have of driving off the spleen and regulating the circulation. "
+      + "Whenever I find myself growing grim about the mouth; whenever it "
+      + "is a damp, drizzly November in my soul; whenever I find myself "
+      + "involuntarily pausing before coffin warehouses, and bringing up "
+      + "the rear of every funeral I meet; and especially whenever my "
+      + "hypos get such an upper hand of me, that it requires a strong "
+      + "moral principle to prevent me from deliberately stepping into "
+      + "the street, and methodically knocking people's hats off-then, "
+      + "I account it high time to get to sea as soon as I can."},
+  function (err, response) {
+    if (err)
+      console.log('error:', err);
+    else
+      console.log(JSON.stringify(response, null, 2));
+});  
 }
 
 ////////////////////////////////////////////////////////
